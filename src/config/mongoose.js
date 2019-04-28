81adadd0ef817d7492166a5c4eb86377a4454aae
Juan Rodas <juan.rodas.manez@gmail.com>
2019-04-28 00:23:55 -0500

    Update Mongoose

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
-const config = require('./config');
-mongoose.Promise = global.Promise;
-mongoose.connect(`${config.mongo.host}:${config.mongo.port}/${config.mongo.db}`, { useNewUrlParser: true }).then(
-    () => {
-        console.log("Database is connected");
-    },
-    err => {
-        console.log("Can not connect to the database'+ err");
-        throw new Error(`unable to connect to database: ${config.mongo.host}`);
-    }
-);
+// const mongoose = require('mongoose');
+// const config = require('./config');
+// mongoose.Promise = global.Promise;
+// mongoose.connect(`${config.mongo.host}:${config.mongo.port}/${config.mongo.db}`, { useNewUrlParser: true }).then(
+//     () => {
+//         console.log("Database is connected");
+//     },
+//     err => {
+//         console.log("Can not connect to the database'+ err");
+//         throw new Error(`unable to connect to database: ${config.mongo.host}`);
+//     }
+// );
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('MongoDB database connection established successfully!');
-});
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log('MongoDB database connection established successfully!');
+// });
