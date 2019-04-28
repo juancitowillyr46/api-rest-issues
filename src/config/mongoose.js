466fa74340b834d6c20b1262e5e330d63d1b7363
Juan Rodas <juan.rodas.manez@gmail.com>
2019-04-28 01:53:17 -0500

    Update mongoose.js

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,17 +1,18 @@
-// const mongoose = require('mongoose');
-// const config = require('./config');
-// mongoose.Promise = global.Promise;
-// mongoose.connect(`${config.mongo.host}:${config.mongo.port}/${config.mongo.db}`, { useNewUrlParser: true }).then(
-//     () => {
-//         console.log("Database is connected");
-//     },
-//     err => {
-//         console.log("Can not connect to the database'+ err");
-//         throw new Error(`unable to connect to database: ${config.mongo.host}`);
-//     }
-// );
+ const mongoose = require('mongoose');
+ const config = require('./config');
+ mongoose.Promise = global.Promise;
+ // ${config.mongo.host}:${config.mongo.port}/${config.mongo.db}
+ mongoose.connect(`mongodb+srv://admin:1CKolbExQc9MQBJktuAN@@cluster0-xiqw7.mongodb.net/test?retryWrites=true`, { useNewUrlParser: true }).then(
+     () => {
+         console.log("Database is connected");
+     },
+     err => {
+         console.log("Can not connect to the database'+ err");
+         throw new Error(`unable to connect to database: ${config.mongo.host}`);
+     }
+ );
 
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//     console.log('MongoDB database connection established successfully!');
-// });
+ const connection = mongoose.connection;
+ connection.once('open', () => {
+     console.log('MongoDB database connection established successfully!');
+ });
