b400630b108e5b30efc68eaec9d7737caa9c1467
Juan Rodas <juan.rodas.manez@gmail.com>
2019-04-28 00:30:07 -0500

    Port

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const config = {
-    port: process.env.SERVER_PORT,
+    port: process.env.SERVER_PORT || 3000,
     mongo: {
         host: process.env.MONGO_HOST,
         port: process.env.MONGO_PORT,
